5728046bb149062fa23c8c99a815ea3a2f99e594
nguyenduy-android <nguyenduy.android@gmail.com>
2020-07-02 16:48:20 +0700

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import request
-import joblib
 import json
 from couchbase.cluster import Cluster, PasswordAuthenticator, ClusterOptions
 from couchbase.auth import PasswordAuthenticator

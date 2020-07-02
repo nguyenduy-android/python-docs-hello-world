a2aa3ed0a561f9419bb29fefff0977190597367f
nguyenduy-android <nguyenduy.android@gmail.com>
2020-07-02 19:22:49 +0700

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask import request
+import json
+from couchbase.cluster import Cluster, PasswordAuthenticator, ClusterOptions
+from couchbase.auth import PasswordAuthenticator
+import base64
+import pickle
+
 app = Flask(__name__)
 
 @app.route("/")
